5f866e59a788ae017b89d93605dc7a7a8ffb8eb6
Dominick D'Emilio <dominickdemilio@email.arizona.edu>
2024-12-14 22:08:14 -0700

    Added new APIs as routes to app.js

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const deviceRoutes = require('./routes/devices');
 
 const app = express();
 const PORT = 8000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Database connection details
-const mongoURI = 'mongodb://127.0.0.1:27017/heart-track'; // Local MongoDB database
+// Connect to MongoDB
+const mongoURI = 'mongodb://127.0.0.1:27017/heart-track'; // Local MongoDB db
 mongoose
     .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('MongoDB connection error:', err));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/devices', deviceRoutes);
 
 app.get('/api/weekly-summary', (req, res) => {
     const summary = {
